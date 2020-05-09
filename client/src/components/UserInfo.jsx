f36c6731b5d2d0c7e35e5733916de9a7c94007d4
alex <axmitchell202@gmail.com>
2020-05-09 02:40:31 -0400

    Convert bandcamp link to hyperlink

--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const UserInfo = props => {
   return (
-    <div id='UserInfo' className='Content'>
+    <div id='UserInfo' className='Content' onClick={() => console.log('clicked')}>
       <div id='UserInfoPhoto'>
         <img src={props.userInfo.photo}></img>
       </div>
-      <div id='UserInfoName'>{props.userInfo.name}</div>
-      <div id='UserInfoLocation'>({props.userInfo.location})</div>
-      <div id='UserInfoLink'>{props.userInfo.link}</div>
+      <div id='UserInfoMain'>
+        <div id='UserInfoName'>{props.userInfo.name}</div>
+        <div id='UserInfoLocation'>({props.userInfo.location})</div>
+        <div id='UserInfoLink'><a href={props.userInfo.link} target="_blank">bandcamp</a></div>
+      </div>
       <div id='UserInfoAbout'>{props.userInfo.about}</div>
     </div>
   )
