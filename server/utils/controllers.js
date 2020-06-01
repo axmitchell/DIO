9364c04d3e2b14f258b193d6075dd1c7034e1e45
alex <axmitchell202@gmail.com>
2020-06-01 16:45:31 -0400

    Include relevant data from user table on set and show requests

--- server/utils/controllers.js
+++ server/utils/controllers.js
@@ -7,24 +7,40 @@ getUser = (req, res) => {
 };
 
 getSets = (req, res) => {
+  db.User.hasMany(db.Set, {foreignKey: 'userId'})
+  db.Set.belongsTo(db.User, {foreignKey: 'userId'})
+
   if (req.params.userId) {
-    db.Set.findAll({where: {userId: req.params.userId }})
+    db.Set.findAll({
+      where: {userId: req.params.userId },
+      include: {model: db.User, attributes: { exclude: ["type", "id", "about", "photo"], required: false }}
+    })
       .then(data => res.send(data))
       .catch(err => res.status(500).send(err));
   } else {
-    db.Set.findAll()
+    db.Set.findAll({
+      include: {model: db.User, attributes: { exclude: ["type", "id", "about", "photo"], required: false }}
+    })
       .then(data => res.send(data))
       .catch(err => res.status(500).send(err));
   }
 };
 
 getShows = (req, res) => {
+  db.User.hasMany(db.Show, {foreignKey: 'userId'})
+  db.Show.belongsTo(db.User, {foreignKey: 'userId'})
+
   if (req.params.userId) {
-    db.Show.findAll({where: {userId: req.params.userId}})
+    db.Show.findAll({
+      where: {userId: req.params.userId},
+      include: {model: db.User, attributes: { exclude: ["type", "id", "about", "photo"], required: false }}
+    })
       .then(data => res.send(data))
       .catch(err => res.status(500).send(err));
   } else {
-    db.Show.findAll()
+    db.Show.findAll({
+      include: {model: db.User, attributes: { exclude: ["type", "id", "about", "photo"], required: false }}
+    })
       .then(data => res.send(data))
       .catch(err => res.status(500).send(err));
   }
